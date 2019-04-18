86bd5a887ab62ac66f2c65bec3f6e5bf9ff24252
Elena Sügis <elena.sugis@gmail.com>
2019-04-18 13:07:59 +0300

    Update create_network_subset.R
    Create disease genes subnetworks of 1hop and 2hop .

--- case_study/scripts/create_network_subset.R
+++ case_study/scripts/create_network_subset.R
@@ -42,3 +42,65 @@ node_attributes_small_net<-node_attributes[node_attributes$ensg%in%ds_ensg,]
 
 save(node_attributes_small_net, file="case_study/datasets/genes_data/node_attributes_small_net.RData")
 #24825
+
+
+print("Extracting 1-hop neighbourhoods of Alzheimer's disease related genes in HENA")
+library(igraph)
+
+### Make a graph from interactions dataframe
+g <- graph_from_data_frame(int_small_net, directed=FALSE)
+
+#V(g)$name <- c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j")
+
+### TODO!!!!!Create a vector of node names that are associated with Alzheimer's disease
+
+node_attributes_small_net$GWAS_pvalue<-as.numeric(node_attributes_small_net$GWAS_pvalue)
+gwas_node_att <- node_attributes_small_net[!is.na(node_attributes_small_net$GWAS_pvalue),c(2)]
+gwas_nodes <- unique(gwas_node_att$ensg)
+
+# List of nodes from IntAct Alzheimer's dataset
+int_alz <- int_small_net[int_small_net$data_source%in%"ADIA",]
+intact_alz_nodes <-unique(c(int_alz$ensg.A, int_alz$ensg.B))
+
+# Combine node lists
+alz_nodes <- c(gwas_nodes, intact_alz_nodes)
+
+### Extract 2HOP neighbourhoods of genes associated with disease
+graph_2hop_alz_subgs <- make_ego_graph(g, 2, alz_nodes)
+
+# Combine individual subgraphs
+g_combined_2hop <- graph_2hop_alz_subgs[[1]]
+for (i in 2:length(graph_2hop_alz_subgs)){
+    g_combined_2hop<-union(g_combined_2hop, graph_2hop_alz_subgs[[i]])
+}
+graph_2hop_alz <- g_combined_2hop
+
+# Check the number of interactions
+gsize(graph_2hop_alz)
+
+# Convert to dataframe from graph structure
+2hop_alz_int <- as_data_frame(graph_2hop_alz, what="edges")
+
+# Save to file as RData and as txt
+save(2hop_alz_int, file="2hop_alz_int.RData")
+write.table(2hop_alz_int, file="case_study/datasets/genes_data/2hop_alz_int.txt", sep="\t", quote=F, row.names = F)
+
+### Extract 1HOP neighbourhoods of genes associated with disease
+graph_1hop_alz_subgs <- make_ego_graph(g, 1, alz_nodes)
+
+# Combine individual subgraphs
+g_combined_1hop <- graph_1hop_alz_subgs[[1]]
+for (i in 2:length(graph_1hop_alz_subgs)){
+    g_combined_1hop<-union(g_combined_1hop, graph_1hop_alz_subgs[[i]])
+}
+graph_1hop_alz <- g_combined_1hop
+
+# Check the number of interactions
+gsize(graph_1hop_alz)
+
+# Convert to dataframe from graph structure
+1hop_alz_int <- as_data_frame(graph_1hop_alz, what="edges")
+
+# Save to file as RData and as txt
+save(1hop_alz_int, file="1hop_alz_int.RData")
+write.table(1hop_alz_int, file="case_study/datasets/genes_data/1hop_alz_int.txt", sep="\t", quote=F, row.names = F)
